0b79a26a42891aae9648edeea76dfe5d1a3e040c
Karol <kar.mal@icloud.com>
2020-01-07 18:23:18 +0000

    js modules structure

---
+++ script.js
@@ -0,0 +1,18 @@
+// Budget app modules architecture and structure
+var budgetController = (function () {
+
+    //Code
+
+})();
+
+var UIController = (function () {
+
+    // Code
+
+})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+
+    //Code
+
+})(budgetController, UIController);
