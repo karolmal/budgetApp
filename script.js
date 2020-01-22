87ef831ac811cbfa86248f9337dbbcb43c9be989
Karol <kar.mal@icloud.com>
2020-01-22 21:25:52 +0000

    clear description and value field after adding an income or expense

--- script.js
+++ script.js
@@ -93,6 +93,20 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
 
+    clearFields: function() {
+      var fields, fieldsArr;
+
+      fields = document.querySelectorAll(
+        DOMstrings.inputDescription + ", " + DOMstrings.inputValue
+      );
+
+      var fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = "";
+      });
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -123,9 +137,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     // 3 Add item to the UI
     UICtrl.addListItem(newItem, input.type);
-    // 4 Calculate the budget
 
-    // 5 Display the budget on the UI
+    // 4 Clear the fields
+    UICtrl.clearFields();
+
+    // 5 Calculate the budget
+
+    // 6 Display the budget on the UI
   };
 
   return {
